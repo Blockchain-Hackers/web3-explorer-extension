7139b4fc0a29d2db46cd1cd1543564ea5dbe1136
Favour <gismozfexie@yahoo.com>
2023-09-05 13:47:38 +0100

    feat: add home component

---
+++ src/pages/home.vue
@@ -0,0 +1,27 @@
+<script setup>
+import IconFire from '~icons/carbon/fire'
+import Link from '~/components/Link.vue'
+
+const quickActionLinks = [
+  { name: 'Upload File', path: '/upload' },
+  { name: 'Connect Account', path: '/connect' },
+  { name: 'View Files', path: '/files' },
+]
+</script>
+
+<template>
+  <section class="h-full w-full flex items-center justify-center flex-col">
+    <h1 class="text-gray-700 font-medium">
+      <strong>Welcome</strong> to FileCoin Explorer
+    </h1>
+    <div class="mt-10">
+      <h2 class="text-center"><icon-fire class="inline-block text-pink-600" /> Quick actions</h2>
+      <div class="flex justify-center flex-wrap gap-2 mt-2 text-sm">
+        <Link
+          v-for="btn in quickActionLinks">
+          {{ btn.name }}
+        </Link>
+      </div>
+    </div>
+  </section>
+</template>
