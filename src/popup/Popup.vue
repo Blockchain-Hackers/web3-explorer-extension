6e27c97495f5abc0f82f6ec7c2559c99c65ae94e
Favour <gismozfexie@yahoo.com>
2023-09-05 15:09:32 +0100

    chore: add make-shift navigation

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 // import { storageDemo } from '~/logic/storage'
 import Account from '~/pages/account.vue'
+import File from '~/pages/file.vue'
+// import FileView from '~/pages/file-view.vue'
+import Home from '~/pages/home.vue'
+import ImportExport from '~/pages/import-export.vue'
 
 const navList = [
-  { name: 'Files', path: '/', active: false },
-  { name: 'Import / Export', path: '/about', active: false },
-  { name: 'Account', path: '/contact', active: true },
+  { name: 'Home', slug: 'home', component: Home },
+  { name: 'File', slug: 'file', component: File },
+  { name: 'Import / Export', slug: 'import-export', component: ImportExport },
+  { name: 'Account', slug: 'account', component: Account },
 ]
+const activeNav = ref(navList[0])
 
 // function openOptionsPage() {
 //   browser.runtime.openOptionsPage()
@@ -19,14 +25,15 @@ const navList = [
       class="w-[100px] min-w-[100px] max-w-[100px]
       border-r-2 border-emerald-600/10"
     >
-      <div class="w-full mt-4 flex justify-center">
-        <Logo class="mx-auto" />
+      <div class="w-full mt-4 flex px-3">
+        <Logo />
       </div>
 
       <div class="w-full mt-4">
         <NuxtLink
           v-for="(nav, i) in navList" :key="i"
-          :class="{ 'bg-emerald-800/30 hover:bg-emerald-800/30': nav.active }"
+          @click="activeNav = nav"
+          :class="{ 'bg-emerald-800/30 hover:bg-emerald-800/30': activeNav.name === nav.name }"
           class="block w-full p-3 border-b
           last-of-type:border-b-none border-emerald-600/10
           hover:bg-emerald-600/10 cursor-pointer text-emerald-900
@@ -37,11 +44,7 @@ const navList = [
       </div>
     </aside>
     <div class="p-2 overflow-y-auto text-gray-600 w-full">
-      <!-- <Home /> -->
-      <!-- <File /> -->
-      <!-- <FileView /> -->
-      <!-- <ImportExport /> -->
-      <Account />
+      <component :is="activeNav.component" />
     </div>
 
     <!-- <Logo />
