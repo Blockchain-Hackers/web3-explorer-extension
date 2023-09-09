e99f7e3db5a858bf28bf582e93b9ab42239168a3
Johnpaul <polymenjohn1@gmail.com>
2023-09-09 17:51:34 +0100

    rename to track files

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 // import { storageDemo } from '~/logic/storage'
-import { activeNav, navList } from '~/logic/route'
+import Account from "~/components/pages/Account.vue";
+import File from "~/components/pages/File.vue";
+// import FileView from '~/components/pages/FileView.vue'
+import Home from "~/components/pages/Home.vue";
+import ImportExport from "~/components/pages/ImportExport.vue";
+
+const navList = [
+  { name: "Home", slug: "home", component: Home },
+  { name: "My Files", slug: "file", component: File },
+  { name: "Tracked Files", slug: "import-export", component: ImportExport },
+  { name: "Account", slug: "account", component: Account },
+];
+const activeNav = ref(navList[0]);
 
 // function openOptionsPage() {
 //   browser.runtime.openOptionsPage()
