0388d004581e88574ce547683fc44cab866cf32e
Favour <gismozfexie@yahoo.com>
2023-09-09 01:06:55 +0100

    chore: chore

--- src/utils/useCIDEncryption.ts
+++ src/utils/useCIDEncryption.ts
@@ -12,3 +12,20 @@ export const encryptionSignature = async () => {
     publicKey: address
   });
 }
+
+const decryptCIDFile = async (cid: string) => {
+  const { publicKey, signedMessage } = await encryptionSignature();
+
+  const keyObject = await lighthouse.fetchEncryptionKey(
+    cid,
+    publicKey,
+    signedMessage
+  );
+
+  const fileType = "image/jpeg";
+  const decrypted = await lighthouse.decryptFile(cid, keyObject.data.key as string, fileType);
+
+  const url = URL.createObjectURL(decrypted);
+  return url;
+
+}
