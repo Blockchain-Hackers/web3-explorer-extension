8dc0e64c10cf5233975bc81153f98dbb2d814532
Johnpaul <polymenjohn1@gmail.com>
2023-09-09 15:14:03 +0100

    connect account feature

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -1,32 +1,81 @@
 <script setup lang="ts">
-import { useStorageLocal } from '~/composables/useStorageLocal'
-import IconRight from '~icons/carbon/chevron-right'
+import { useStorageLocal, storageLocal } from "~/composables/useStorageLocal";
+import IconRight from "~icons/carbon/chevron-right";
+import lighthouse from "@lighthouse-web3/sdk";
 
-const apiKey = useStorageLocal('apiKey', '')
-const isLoggedIn = ref(false)
-const text = computed(()=>{
-  return isLoggedIn.value ? {
-    title: 'Disconnect Web3 Storage Account',
-    description: 'Your Files will still be visible but you cannot renew or repair them',
-    button: 'Disconnect'
-  } : {
-    title: 'Connect Web3 Storage Account',
-    description: 'You can only perform actions like repair and renew after connecting your account',
-    button: 'Connect'
+const _apiKey = useStorageLocal("apiKey", "");
+const apiKey = ref(_apiKey.value);
+const isLoggedIn = ref(!!apiKey.value);
+const notifications = ref({
+  showError: false,
+  showSuccess: false,
+});
+console.log(apiKey);
+const text = computed(() => {
+  return isLoggedIn.value
+    ? {
+        title: "Disconnect Lighthouse Account",
+        description:
+          "Your Files will still be visible but you cannot renew or repair them",
+        button: "Disconnect",
+      }
+    : {
+        title: "Connect Lighthouse Account",
+        description:
+          "You can only perform actions like repair and renew after connecting your account",
+        button: "Connect",
+      };
+});
+
+watch(_apiKey, (value) => {
+  isLoggedIn.value = !!value;
+});
+
+watch(apiKey, (value) => {
+  notifications.value.showError = false;
+});
+
+const handleSubmit = async () => {
+  // if storage apikey is set, remove; else set
+
+  // logout
+  if (_apiKey.value) {
+    storageLocal.removeItem("apiKey");
+    _apiKey.value = "";
+    return;
   }
-})
 
-const handleSubmit = ()=>{
-  apiKey.value = ''
-  isLoggedIn.value = !isLoggedIn.value
-}
+  // try to login
+  let balance: any;
+  try {
+    balance = await lighthouse.getUploads(apiKey.value);
+    console.log(balance);
+
+    // Display the balance details
+
+    storageLocal.setItem("apiKey", apiKey.value);
+    _apiKey.value = apiKey.value;
+    notifications.value.showSuccess = true;
+  } catch (error) {
+    console.log(error);
+    notifications.value.showError = true;
+  }
+};
 </script>
 
 <template>
   <div class="h-full">
     <section class="h-full w-full flex flex-col">
-      <h2 class="font-bold"><icon-right class="inline-block" /> {{ text.title }}</h2>
+      <h2 class="font-bold">
+        <icon-right class="inline-block" /> {{ text.title }}
+      </h2>
       <p class="pl-4 text-gray-500 font-medium">{{ text.description }}</p>
+      <p
+        v-if="notifications.showSuccess"
+        class="pl-4 text-green-500 font-medium"
+      >
+      Your account has been connected successfully
+    </p>
       <div class="flex-grow flex mt-10">
         <form @submit.prevent="handleSubmit" class="w-full">
           <label v-if="!isLoggedIn" class="relative block">
@@ -34,14 +83,20 @@ const handleSubmit = ()=>{
             <input
               v-model="apiKey"
               type="text"
-              placeholder="0x23cskicsslskllllkdlbicbhsxsale"
+              placeholder="xxxxxxx.XXXXXXXXXXXXXXXXXXXXXXXX"
               required
-              class="w-full bg-gray-100 p-4 pt-7 rounded-md outline-emerald-700/30 text-base">
+              class="w-full bg-gray-100 p-4 pt-7 rounded-md outline-emerald-700/30 text-base"
+            />
           </label>
-          
+
+          <div v-if="notifications.showError" class="text-red-500 text-sm mt-2">
+            Invalid API key
+          </div>
+
           <button
-            class="inline-block p-3 bg-emerald-600/10 rounded-md text-black font-medium mt-3
-            hover:bg-emerald-700/30 transition-all duration-300 cursor-pointer text-base w-full">
+            class="inline-block p-3 bg-emerald-600/10 rounded-md text-black font-medium mt-3 hover:bg-emerald-700/30 transition-all duration-300 cursor-pointer text-base w-full"
+            type="submit"
+          >
             {{ text.button }}
           </button>
         </form>
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 // import { storageDemo } from '~/logic/storage'
-import Account from '~/components/pages/Account.vue'
-import File from '~/components/pages/File.vue'
+import Account from "~/components/pages/Account.vue";
+import File from "~/components/pages/File.vue";
 // import FileView from '~/components/pages/FileView.vue'
-import Home from '~/components/pages/Home.vue'
-import ImportExport from '~/components/pages/ImportExport.vue'
+import Home from "~/components/pages/Home.vue";
+import ImportExport from "~/components/pages/ImportExport.vue";
 
 const navList = [
-  { name: 'Home', slug: 'home', component: Home },
-  { name: 'File', slug: 'file', component: File },
-  { name: 'Import / Export', slug: 'import-export', component: ImportExport },
-  { name: 'Account', slug: 'account', component: Account },
-]
-const activeNav = ref(navList[0])
+  { name: "Home", slug: "home", component: Home },
+  { name: "File", slug: "file", component: File },
+  { name: "Import / Export", slug: "import-export", component: ImportExport },
+  { name: "Account", slug: "account", component: Account },
+];
+const activeNav = ref(navList[0]);
 
 // function openOptionsPage() {
 //   browser.runtime.openOptionsPage()
@@ -22,8 +22,7 @@ const activeNav = ref(navList[0])
 <template>
   <main class="w-[400px] h-[400px] flex bg-gray-50">
     <aside
-      class="w-[100px] min-w-[100px] max-w-[100px]
-      border-r-2 border-emerald-600/10"
+      class="w-[100px] min-w-[100px] max-w-[100px] border-r-2 border-emerald-600/10"
     >
       <div class="w-full mt-4 flex px-3">
         <Logo />
@@ -31,13 +30,14 @@ const activeNav = ref(navList[0])
 
       <div class="w-full mt-4">
         <NuxtLink
-          v-for="(nav, i) in navList" :key="i"
+          v-for="(nav, i) in navList"
+          :key="i"
           @click="activeNav = nav"
-          :class="{ 'bg-emerald-800/30 hover:bg-emerald-800/30': activeNav.name === nav.name }"
-          class="block w-full p-3 border-b
-          last-of-type:border-b-none border-emerald-600/10
-          hover:bg-emerald-600/10 cursor-pointer text-emerald-900
-          transition-all duration-200 font-bold"
+          :class="{
+            'bg-emerald-800/30 hover:bg-emerald-800/30':
+              activeNav.name === nav.name,
+          }"
+          class="block w-full p-3 border-b last-of-type:border-b-none border-emerald-600/10 hover:bg-emerald-600/10 cursor-pointer text-emerald-900 transition-all duration-200 font-bold"
         >
           {{ nav.name }}
         </NuxtLink>
