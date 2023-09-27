20a9d9c475d6da780593049b2b6df7f26efa9eda
Favour <gismozfexie@yahoo.com>
2023-09-27 10:00:24 +0100

    chore: make page content scrollable

--- src/components/pages/FileView.vue
+++ src/components/pages/FileView.vue
@@ -58,112 +58,115 @@ onMounted(() => accessConditions(fileDetails.cid));
 
 <template>
   <div class="w-full h-full flex flex-col">
-    <div class="bg-gray-100 h-20 rounded-lg overflow-hidden shadow">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-full h-full object-cover"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-        />
-      </svg>
-    </div>
-
+    
     <div class="pt-2 flex-grow flex flex-col">
-      <div class="flex-grow flex flex-col">
-        <div class="flex flex-col">
-          <p
-            :class="{ '!text-emerald-600': copied }"
-            class="truncate flex transition-all duration-300 cursor-pointer pr-2 hover:!text-emerald-600"
-            @click="CopyText(cId)"
-          >
-            CID:
-            <strong
-              :class="{ '!text-emerald-600': copied }"
-              class="text-black truncate"
-              >{{ cId }}</strong
+      <div class="flex-grow flex flex-col overflow-y-auto relative">
+        <div class="absolute inset-0 overflow-y-auto">
+          <div class="bg-gray-100 h-20 rounded-lg overflow-hidden shadow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-full h-full object-cover"
             >
-            <icon-copy class="inline ml-1 min-w-6" />
-          </p>
-
-          <div>
-            <template v-for="detail in details">
-              <p
-                v-if="detail.name !== 'CID'"
-                class="flex justify-between border-b last-of-type:border-b-none py-1"
-              >
-                {{ detail.name }}:
-                <strong class="text-black ml-1" style="display: flex">
-                  {{ detail.value }}&nbsp;
-                  <span v-if="detail.name === 'Encryption'">
-                    <svg
-                      v-if="detail.value"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-4 h-4"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                      />
-                    </svg>
-                    <svg
-                      v-else
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-4 h-4"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                      />
-                    </svg>
-                  </span>
-                </strong>
-              </p>
-            </template>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              />
+            </svg>
           </div>
-        </div>
 
-        <form @submit.prevent="shareFile" class="w-full flex gap-2 mt-2">
-          <label class="relative block flex-1">
-            <input
-              v-model="shareeAddress"
-              type="text"
-              placeholder="Enter Sharee Address"
-              required
-              class="w-full bg-gray-100 p-4 py-2 rounded-md outline-none ring ring-inset ring-emerald-700 text-base"
-            />
-          </label>
-
-          <button class="block bg-emerald-600 p-2 px-4 rounded-md text-white">
-            Share
-          </button>
-        </form>
-
-        <h3 class="my-3">Addresses file is shared with</h3>
-        <div class="flex-grow relative mt-1">
-          <div class="overflow-y-auto mb-3">
-            <div v-for="address in sharedTo">{{ address }}</div>
+          <div class="flex flex-col mt-2">
+            <p
+              :class="{ '!text-emerald-600': copied }"
+              class="truncate flex transition-all duration-300 cursor-pointer pr-2 hover:!text-emerald-600"
+              @click="CopyText(cId)"
+            >
+              CID:
+              <strong
+                :class="{ '!text-emerald-600': copied }"
+                class="text-black truncate"
+                >{{ cId }}</strong
+              >
+              <icon-copy class="inline ml-1 min-w-6" />
+            </p>
+  
+            <div>
+              <template v-for="detail in details">
+                <p
+                  v-if="detail.name !== 'CID'"
+                  class="flex justify-between border-b last-of-type:border-b-none py-1"
+                >
+                  {{ detail.name }}:
+                  <strong class="text-black ml-1" style="display: flex">
+                    {{ detail.value }}&nbsp;
+                    <span v-if="detail.name === 'Encryption'">
+                      <svg
+                        v-if="detail.value"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-4 h-4"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                        />
+                      </svg>
+                      <svg
+                        v-else
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-4 h-4"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                        />
+                      </svg>
+                    </span>
+                  </strong>
+                </p>
+              </template>
+            </div>
+          </div>
+  
+          <form @submit.prevent="shareFile" class="w-full flex gap-2 mt-2">
+            <label class="relative block flex-1">
+              <input
+                v-model="shareeAddress"
+                type="text"
+                placeholder="Enter Sharee Address"
+                required
+                class="w-full bg-gray-100 p-4 py-2 rounded-md outline-none ring ring-inset ring-emerald-700 text-base"
+              />
+            </label>
+  
+            <button class="block bg-emerald-600 p-2 px-4 rounded-md text-white">
+              Share
+            </button>
+          </form>
+  
+          <h3 class="my-3">Addresses file is shared with</h3>
+          <div class="flex-grow relative mt-1">
+            <div class="overflow-y-auto mb-3">
+              <div v-for="address in sharedTo">{{ address }}</div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="flex gap-2 justify-between text-sm">
+      <div class="flex gap-2 justify-between text-sm pt-2">
         <Link
           v-for="option in options"
           class="w-full text-center"
